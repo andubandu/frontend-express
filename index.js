28bd2bdb7f7f1e0a637ed8116a9020a58ec5dfb0
andubandu <lolandu575@gmail.com>
2025-03-19 14:08:47 +0400

    vercel friendly update?

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');   
+    res.status(200).render('index');
 });
 
-app.listen(3000, () => {
-    console.log('Listening on http://localhost:3000');
-});
+
+module.exports = app;
