437080ffb453585b13ed3e1ea14618713add80e5
andubandu <lolandu575@gmail.com>
2025-03-19 14:29:43 +0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const path = require('path');  
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', './views');  
+
+app.set('views', path.join(__dirname, '../views')); 
+
 app.get('/', (req, res) => {
   res.status(200).render('index');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack); 
+  res.status(500).send('Something went wrong!');
 });
 
 module.exports = (req, res) => app(req, res);
