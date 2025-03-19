5d6c34cd4fba85f45af0d9526e59b7afb0fc3ba3
andubandu <lolandu575@gmail.com>
2025-03-19 14:28:00 +0400

    Vercel resolving

--- api/index.js
+++ api/index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-
+app.set('views', './views');  
 app.get('/', (req, res) => {
-    res.status(200).render('index');
+  res.status(200).render('index');
 });
 
-
-module.exports = app;
+module.exports = (req, res) => app(req, res);
