e6ae879baefa950774515aa5b22689c4ee6ec024
andubandu <lolandu575@gmail.com>
2025-03-19 15:45:11 +0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,9 @@ app.set('views', path.join(__dirname, '../views'));
 app.get('/', (req, res) => {
   res.status(200).render('index');
 });
-
+app.get('/profile', (req, res) => {
+    res.status(200).render('profile');
+})
 app.use((err, req, res, next) => {
   console.error(err.stack); 
   res.status(500).send('Something went wrong!');
